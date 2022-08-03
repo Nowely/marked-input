778658c54863ad411861f3ae8f68943395532eca
Ilyin Ruslan <ilyin-r@mail.ru>
2022-08-03 15:39:54 +0300

    Add sketch of headless component

--- lib/MarkedInput/hooks/useInput.tsx
+++
@@ -1,8 +0,0 @@
-import {Option, OptionProps} from "../../Option";
-import {MarkedInput} from "../MarkedInput";
-
-//TODO hook initialize way
-const useInput = () => {
-    //const A = Option as (props: OptionProps<ButtonProps>) => null
-    //return {MarkedInput, A}
-}
