710564fae98b33056f10a332960c92d07b1293ca
Ilyin Ruslan <ilyin-r@mail.ru>
2022-08-04 17:55:52 +0300

    Add mention MUI story

--- stories/Mui.stories.tsx
+++ stories/Mui.stories.tsx
@@ -1,6 +1,6 @@
 import {MarkedInput, Option} from "../lib";
 import {useState} from "react";
-import {Chip} from "@mui/material";
+import {Avatar, Chip, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography} from "@mui/material";
 import {ChipProps} from "@mui/material/Chip/Chip";
 import {Text} from "./assets/Text";
 import {getTitleOfStyled} from "./assets/getTitle";
@@ -32,4 +32,98 @@ export const Chipped = () => {
 
         <Text value={value}/>
     </>
+}
+
+export const Mention = () => {
+    const [value, setValue] = useState("Hello @[Agustina](A) and @[Frank Parker](FP)")
+
+    return <>
+        <MarkedInput
+            Mark={Chip}
+            value={value}
+            onChange={(val: string) => setValue(val)}
+            Overlay={AlignItemsList}
+        >
+            <Option<ChipProps>
+                trigger="@"
+                markup="@[__value__](__id__)"
+                initializer={(label, id) => ({label, avatar: <Avatar>{id}</Avatar>, size: "small"})}
+            />
+        </MarkedInput>
+
+        <Text value={value}/>
+    </>
+}
+
+//Component based on https://mui.com/material-ui/react-list/#align-list-items
+function AlignItemsList({onSelect}: any) {
+    return (
+        <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+            <ListItem alignItems="flex-start" onClick={() => onSelect({value: "Remy Sharp", id: "RS"})}>
+                <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" children="RS"/>
+                </ListItemAvatar>
+                <ListItemText
+                    primary="Brunch this weekend?"
+                    secondary={
+                        <>
+                            <Typography
+                                sx={{display: 'inline'}}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                Ali Connors
+                            </Typography>
+                            {" — I'll be in your neighborhood doing errands this…"}
+                        </>
+                    }
+                />
+            </ListItem>
+            <Divider variant="inset" component="li"/>
+            <ListItem alignItems="flex-start" onClick={() => onSelect({value: "Travis Howard", id: "TH"})}>
+                <ListItemAvatar>
+                    <Avatar alt="Travis Howard" children="TH"/>
+                </ListItemAvatar>
+                <ListItemText
+                    primary="Summer BBQ"
+                    secondary={
+                        <>
+                            <Typography
+                                sx={{display: 'inline'}}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                to Scott, Alex, Jennifer
+                            </Typography>
+                            {" — Wish I could come, but I'm out of town this…"}
+                        </>
+                    }
+                />
+            </ListItem>
+            <Divider variant="inset" component="li"/>
+            <ListItem alignItems="flex-start" onClick={() => onSelect({value: "Cindy Baker", id: "CB"})}>
+                <ListItemAvatar>
+                    <Avatar alt="Cindy Baker" children="CB"/>
+                </ListItemAvatar>
+                <ListItemText
+                    primary="Oui Oui"
+                    secondary={
+                        <>
+                            <Typography
+                                sx={{display: 'inline'}}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                Sandra Adams
+                            </Typography>
+                            {' — Do you have Paris recommendations? Have you ever…'}
+                        </>
+                    }
+                />
+            </ListItem>
+        </List>
+    );
 }
