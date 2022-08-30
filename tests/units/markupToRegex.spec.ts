0756affe4f0bb5ef32aa9b560a8b1095bde49db4
Ilyin Ruslan <ilyin-r@mail.ru>
2022-08-30 18:01:54 +0300

    Fix createRandomMarkup util

--- tests/units/markupToRegex.spec.ts
+++ tests/units/markupToRegex.spec.ts
@@ -4,13 +4,16 @@ import {createRandomMarkup} from "../utils/createRandomMarkup";
 
 describe(`Utility: ${markupToRegex.name}`, () => {
     it('should convert markup to regex', () => {
-        const markup = createRandomMarkup()
+        const word1 = faker.lorem.word()
+        const word2 = faker.lorem.word()
+
+        const markup = createRandomMarkup(word1 + word2)
         const regex = markupToRegex(markup)
-        const annotation = annotate(markup, faker.lorem.words(), faker.lorem.words())
-        const textWithAnnotation = faker.lorem.words() + annotation +faker.lorem.words()
+        const annotation = annotate(markup, word1, word2)
+        const textWithAnnotation = `${faker.lorem.words()} ${annotation} ${faker.lorem.words()}`
         const textWithoutAnnotation = faker.lorem.sentences()
+
         expect(regex.test(textWithAnnotation)).toBeTruthy()
         expect(regex.test(textWithoutAnnotation)).toBeFalsy()
     });
 })
-
--- tests/utils/createRandomMarkup.ts
+++ tests/utils/createRandomMarkup.ts
@@ -1,9 +1,11 @@
 import {faker} from "@faker-js/faker";
 import {Markup} from "../../lib/MarkedInput/types";
 
-export const createRandomMarkup = () => {
+export const createRandomMarkup = (excludeSymbols: string) => {
     const str1 = faker.datatype.string(5)
     const str2 = faker.datatype.string(5)
     const str3 = faker.datatype.string(5)
-    return str1 + "__label__" + str2 + "__value__" + str3 as Markup
-};
+    const markup = str1 + "__label__" + str2 + "__value__" + str3;
+    const regExp = new RegExp(`[${excludeSymbols}]`, "g");
+    return markup.replace(regExp, '') as Markup
+}
