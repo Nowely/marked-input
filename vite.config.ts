aa18da6b49a2a69798642e3e2dc083140b9ac5c3
Ruslan <ilyin-r@mail.ru>
2022-09-09 16:34:23 +0300

    Rename dist main files

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
             entry: path.resolve(__dirname, 'lib/index.ts'),
             name: 'MarkedInput',
             formats: ['es', 'umd'],
+            fileName: 'index'
         },
         rollupOptions: {
             external: ['react'],
