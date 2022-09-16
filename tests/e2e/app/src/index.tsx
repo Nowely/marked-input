bff48a0ac44ca426b144a5ceff4ea2a5df367daa
Ruslan <ilyin-r@mail.ru>
2022-09-16 23:04:11 +0300

    Add the clear e2e testing app

---
+++ tests/e2e/app/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Configured} from "storybook/stories/Base.stories"
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <Configured/>
+    </React.StrictMode>
+)
