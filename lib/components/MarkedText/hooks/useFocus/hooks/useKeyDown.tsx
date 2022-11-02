29d05d242d070fd24bc08a709a752c326e7abd11
Ilyin Ruslan <ilyin-r@mail.ru>
2022-11-02 16:09:48 +0300

    Refact useKeyDown

--- lib/components/MarkedText/hooks/useFocus/hooks/useKeyDown.tsx
+++ lib/components/MarkedText/hooks/useFocus/hooks/useKeyDown.tsx
@@ -1,84 +1,70 @@
-import {KeyboardEvent, MutableRefObject, RefObject, useEffect} from "react";
+import {KeyboardEvent, MutableRefObject} from "react";
 import {Caret} from "../../../../../utils/Caret";
 import {KEY} from "../../../../../constants";
 import {NodeData, Type} from "../../../../../types";
 import {useStore} from "../../../../../utils";
 import {Recovery} from "./useRecoveryAfterRemove";
-import {useListener} from "../../../../../utils/useListener";
 import LinkedListNode from "../../../../../utils/LinkedListNode";
+import {useDownOf} from "./useDownOf";
 
-//TODO clean up
 export function useKeyDown(
     recoveryRef: MutableRefObject<Recovery | null>,
     focusedNodeRef: MutableRefObject<LinkedListNode<NodeData> | undefined>
 ) {
     const {bus} = useStore()
 
-    useListener("onKeyDown", (event: KeyboardEvent<HTMLSpanElement>) => {
-        const target = event.target as HTMLSpanElement
-        const caretIndex = Caret.getCaretIndex(target);
-        const isStartCaret = caretIndex === 0;
-        const isEndCaret = caretIndex === target.textContent?.length;
+    useDownOf(KEY.LEFT, event => {
+        if (!isCaretInStart(event)) return
 
-        const handleMap = {
-            [KEY.LEFT]: isStartCaret ? handlePressLeft : null,
-            [KEY.RIGHT]: isEndCaret ? handlePressRight : null,
-            [KEY.UP]: null, //TODO to the start input position?
-            [KEY.DOWN]: null, //TODO to the end input position?
-            [KEY.DELETE]: isEndCaret ? handlePressDelete : null,
-            [KEY.BACKSPACE]: isStartCaret ? handlePressBackspace : null,
-        }
+        const node = focusedNodeRef.current?.prev
+        const element = node?.data.ref?.current ?? node?.prev?.data.ref?.current
+        element?.focus()
+        Caret.setCaretToEnd(element)
+        event.preventDefault()
+    })
 
-        handleMap[event.key]?.()
+    useDownOf(KEY.RIGHT, event => {
+        if (!isCaretInEnd(event)) return
 
-        function handlePressLeft() {
-            const node = focusedNodeRef.current?.prev
+        const node = focusedNodeRef.current?.next
+        const element = node?.data.ref?.current ?? node?.next?.data.ref?.current
+        element?.focus()
+        event.preventDefault()
+    })
 
-            //TODO offset for focused text?
-            /*if (node?.data.ref?.current && node.data.ref.current.textContent) {
-                node.data.ref.current.focus()
-                Caret.trySetIndex(node.data.ref.current, node.data.ref.current.textContent.length - 1)
-            } else {*/
-            const element = node?.data.ref?.current ?? node?.prev?.data.ref?.current
-            element?.focus()
-            Caret.setCaretToEnd(element)
-            //}
+    useDownOf(KEY.DELETE, event => {
+        if (!isCaretInEnd(event)) return
 
-            event.preventDefault()
-        }
+        const node = focusedNodeRef.current?.next
+        if (!node?.data.key) return
 
-        function handlePressRight() {
-            const node = focusedNodeRef.current?.next
+        const caretPosition = node.prev?.data.mark.label.length ?? 0
+        recoveryRef.current = {prevNodeData: node.prev?.prev?.data, caretPosition}
+        bus.send(Type.Delete, {key: node.data.key})
+        event.preventDefault()
+    })
 
-            /*if (node?.data.ref?.current && node.data.ref.current.textContent) {
-                node.data.ref.current.focus()
-                Caret.trySetIndex(node.data.ref.current, 1)
-            } else {*/
-            const element = node?.data.ref?.current ?? node?.next?.data.ref?.current
-            element?.focus()
-            //}
+    useDownOf(KEY.BACKSPACE, event => {
+        if (!isCaretInStart(event)) return
 
-            event.preventDefault()
-        }
+        const node = focusedNodeRef.current?.prev
+        if (!node?.data.key) return
 
-        function handlePressBackspace() {
-            const node = focusedNodeRef.current?.prev
-            if (!node?.data.key) return
+        const caretPosition = node.prev?.data.mark.label.length ?? 0
+        recoveryRef.current = {prevNodeData: node.prev?.prev?.data, caretPosition}
+        bus.send(Type.Delete, {key: node.data.key})
+        event.preventDefault()
+    })
+}
 
-            const caretPosition = node.prev?.data.mark.label.length ?? 0
-            recoveryRef.current = {prevNodeData: node.prev?.prev?.data, caretPosition}
-            bus.send(Type.Delete, {key: node.data.key})
-            event.preventDefault()
-        }
+function isCaretInStart(e: KeyboardEvent<HTMLSpanElement>) {
+    const target = e.target as HTMLSpanElement
+    const caretIndex = Caret.getCaretIndex(target);
+    return caretIndex === 0;
+}
 
-        function handlePressDelete() {
-            const node = focusedNodeRef.current?.next
-            if (!node?.data.key) return
-
-            const caretPosition = node.prev?.data.mark.label.length ?? 0
-            recoveryRef.current = {prevNodeData: node.prev?.prev?.data, caretPosition}
-            bus.send(Type.Delete, {key: node.data.key})
-            event.preventDefault()
-        }
-    }, [])
+function isCaretInEnd(event: KeyboardEvent<HTMLSpanElement>) {
+    const target = event.target as HTMLSpanElement
+    const caretIndex = Caret.getCaretIndex(target);
+    return caretIndex === target.textContent?.length;
 }
