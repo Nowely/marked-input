de758e99c7481ac040c2d1491a7f62b31f91d7ec
Ilyin Ruslan <ilyin-r@mail.ru>
2022-11-16 12:39:24 +0300

    Fix test

--- tests/integrations/createMarkedInput.spec.tsx
+++ tests/integrations/createMarkedInput.spec.tsx
@@ -4,10 +4,8 @@ import {act, render} from "@testing-library/react";
 import {Configured} from "storybook/stories/Base.stories";
 import React, {forwardRef, useState} from "react";
 import {createMarkedInput} from "rc-marked-input";
-import {MarkProps} from "rc-marked-input/types";
 import {vi} from "vitest";
 
-//TODO
 describe(`Utility: createMarkedInput`, () => {
     it('should render', () => {
         render(<Configured/>)
@@ -19,27 +17,24 @@ describe(`Utility: createMarkedInput`, () => {
         document.addEventListener = vi.fn((event, callback) => events[event] = callback);
         document.removeEventListener = vi.fn((event, callback) => delete events[event]);
 
-        const {container, debug, queryByText} = render(<Mark3/>)
-        const [span] = container.querySelectorAll("span")
-        await user.type(span, '@1')
-        await user.type(span, '{ArrowLeft}', {initialSelectionStart: 0})
-        console.log(span)
-
+        const {queryByText, getByText} = render(<Mark3/>)
+        const span = getByText(/hello/i)
+        await user.type(span, '{ArrowRight}')
         expect(span).toHaveFocus()
+
         await act(() => {
             // @ts-ignore
             events['selectionchange']()
         });
 
-        debug()
         expect(await queryByText("I'm here!")).toBeInTheDocument()
     })
 })
 
 const Mark3 = () => {
-    const [value, setValue] = useState('1')
+    const [value, setValue] = useState('Hello @')
     const Overlay = forwardRef(() => <span>I'm here!</span>)
-    const Input = createMarkedInput(props => <mark>{props.label}</mark>, Overlay, [])
+    const Input = createMarkedInput(() => null, Overlay, [{}])
 
     return <Input value={value} onChange={setValue}/>
 }
