bb3edf9cf9443d6215fb7c754edca2204a7363a8
Ruslan <ilyin-r@mail.ru>
2022-11-16 03:41:11 +0300

    Update material mentions story

--- storybook/stories/Material.stories.tsx
+++ storybook/stories/Material.stories.tsx
@@ -1,10 +1,10 @@
 import {MarkedInput, Option} from "rc-marked-input";
 import {useState} from "react";
-import {Avatar, Chip, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography} from "@mui/material";
+import {Chip} from "@mui/material";
 import {ChipProps} from "@mui/material/Chip/Chip";
 import {Text} from "./assets/Text";
 import {getTitle} from "./assets/getTitle";
-import {OverlayProps} from "rc-marked-input/types";
+import {MaterialMentions} from "./assets/MaterialMentions";
 
 export default {
     title: getTitle("Material"),
@@ -35,96 +35,12 @@ export const Chipped = () => {
     </>
 }
 
-export const Mention = () => {
-    const [value, setValue] = useState(
-        `Enter the '@' for calling mention list: \n- Hello @[Agustina](A) and @[Frank Parker](FP)!`
-    )
+export const Mentions = () => {
+    const [value, setValue] = useState(`Enter the '@' for calling mention list: \n- Hello @Agustina and @[Ruslan]!`)
 
     return <>
-        <MarkedInput
-            Mark={Chip}
-            value={value}
-            onChange={(val: string) => setValue(val)}
-            Overlay={AlignItemsList}
-        >
-            <Option<ChipProps>
-                initMark={({label, value}) => ({label, avatar: <Avatar>{value}</Avatar>, size: "small"})}
-            />
-        </MarkedInput>
+        <MaterialMentions value={value} onChange={setValue}/>
 
         <Text label="Plaint text:" value={value}/>
     </>
-}
-
-//Component based on https://mui.com/material-ui/react-list/#align-list-items
-function AlignItemsList({onSelect}: OverlayProps) {
-    return (
-        <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
-            <ListItem alignItems="flex-start" onClick={() => onSelect({label: "Remy Sharp", value: "RS"})}>
-                <ListItemAvatar>
-                    <Avatar alt="Remy Sharp" children="RS"/>
-                </ListItemAvatar>
-                <ListItemText
-                    primary="Brunch this weekend?"
-                    secondary={
-                        <>
-                            <Typography
-                                sx={{display: 'inline'}}
-                                component="span"
-                                variant="body2"
-                                color="text.primary"
-                            >
-                                Ali Connors
-                            </Typography>
-                            {" — I'll be in your neighborhood doing errands this…"}
-                        </>
-                    }
-                />
-            </ListItem>
-            <Divider variant="inset" component="li"/>
-            <ListItem alignItems="flex-start" onClick={() => onSelect({label: "Travis Howard", value: "TH"})}>
-                <ListItemAvatar>
-                    <Avatar alt="Travis Howard" children="TH"/>
-                </ListItemAvatar>
-                <ListItemText
-                    primary="Summer BBQ"
-                    secondary={
-                        <>
-                            <Typography
-                                sx={{display: 'inline'}}
-                                component="span"
-                                variant="body2"
-                                color="text.primary"
-                            >
-                                to Scott, Alex, Jennifer
-                            </Typography>
-                            {" — Wish I could come, but I'm out of town this…"}
-                        </>
-                    }
-                />
-            </ListItem>
-            <Divider variant="inset" component="li"/>
-            <ListItem alignItems="flex-start" onClick={() => onSelect({label: "Cindy Baker", value: "CB"})}>
-                <ListItemAvatar>
-                    <Avatar alt="Cindy Baker" children="CB"/>
-                </ListItemAvatar>
-                <ListItemText
-                    primary="Oui Oui"
-                    secondary={
-                        <>
-                            <Typography
-                                sx={{display: 'inline'}}
-                                component="span"
-                                variant="body2"
-                                color="text.primary"
-                            >
-                                Sandra Adams
-                            </Typography>
-                            {' — Do you have Paris recommendations? Have you ever…'}
-                        </>
-                    }
-                />
-            </ListItem>
-        </List>
-    );
 }
---
+++ storybook/stories/assets/MaterialMentions/Mention.tsx
@@ -0,0 +1,13 @@
+import {MarkProps} from "rc-marked-input/types/types";
+import {Avatar, Chip} from "@mui/material";
+import {useFetch} from "./utils/useFetch";
+import {User} from "./types";
+
+export const Mention = ({label}: MarkProps) => {
+    const [user] = useFetch<User>(`https://api.github.com/users/${label}`, [])
+    const abbr = getAbbr(user?.name) ?? label[0]
+
+    return <Chip variant="outlined" label={label} size="small" avatar={<Avatar src={user?.avatar_url} children={abbr}/>}/>
+}
+
+export const getAbbr = (str?: string, length: number = 3) => str?.split(' ').slice(0, length).map(s => s[0]).join('')
