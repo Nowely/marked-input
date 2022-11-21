2ef6bc8684ae50d67a6db40df3d236182ac8e11e
Ruslan <ilyin-r@mail.ru>
2022-11-21 23:36:06 +0300

    Add checks

--- lib/components/Featurer/hooks/useCloseOverlayByOutsideClick.tsx
+++ lib/components/Featurer/hooks/useCloseOverlayByOutsideClick.tsx
@@ -1,11 +1,15 @@
 import {RefObject, useEffect} from "react";
 import {useStore} from "../../../utils";
 import {Type} from "../../../types";
+import {useSelector} from "../../../utils/useSelector";
 
 export function useCloseOverlayByOutsideClick() {
     const store = useStore()
+    const trigger = useSelector(state => state.trigger)
 
     useEffect(() => {
+        if (!trigger) return
+
         const handleClick = (event: MouseEvent) => {
             let target = event.target as HTMLElement | null
             if (store.overlayRef.current?.contains(target) || store.containerRef.current?.contains(target)) return
@@ -14,5 +18,5 @@ export function useCloseOverlayByOutsideClick() {
 
         document.addEventListener("click", handleClick)
         return () => document.removeEventListener("click", handleClick)
-    }, [])
+    }, [trigger])
 }
