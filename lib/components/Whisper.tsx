da9210ec29d5e59dd0e3e630338d56e470d5628b
Ruslan <ilyin-r@mail.ru>
2023-03-29 01:24:05 +0300

    Fix overlay

--- lib/components/Whisper.tsx
+++ lib/components/Whisper.tsx
@@ -3,10 +3,11 @@ import {useSelector} from '../utils/useSelector'
 import {Suggestions} from './Suggestions'
 
 export const Whisper = memo(() => {
-	const key = useSelector(state => state.options.indexOf(state.overlayMatch?.option ?? {}))
+	// @ts-ignore TODO
+	const key = useSelector(state => state.overlayMatch?.option.index)
 	const Overlay = useSelector(state => state.Overlay ?? Suggestions)
 
-	return key !== -1 ? <Overlay key={key}/> : null
+	return key !== undefined ? <Overlay key={key}/> : null
 })
 
 Whisper.displayName = 'Whisper'
