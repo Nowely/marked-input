cb3ed90518ef5c56bfead7f6c02bff1ab8a28186
Ruslan <ilyin-r@mail.ru>
2023-05-25 16:01:26 +0300

    Add patience Diff

--- tests/benchmarks/index.ts
+++ tests/benchmarks/index.ts
@@ -1,3 +1,29 @@
 import {Benchmark} from './_utils/Benchmark'
+import {genBNFGrammar} from './_utils/genBNFGrammar'
+import {getData} from './_utils/getData'
+import {BNFParser} from './_utils/parsers/BNFParser'
+import {patienceDiff, patienceDiffPlus} from './_utils/PatienceDiff'
+
 
 //await Benchmark.start(1)
+//await Benchmark.continue(1)
+//await genBNFGrammar()
+
+const ab = 'Hello world!'.split('')
+const ac = 'World Hello!'.split('')
+
+const a = patienceDiffPlus(ab, ac)
+//const a1 = patienceDiff(ab, ac, false)
+//const a2 = patienceDiff(ab, ac, true)
+//console.log(a)
+//console.log(a1)
+//console.log(a2)
+debugger
+
+//console.log(grammar)
+
+
+//const data = await getData(18)
+//debugger
+//const parser = new BNFParser([])
+//const a = parser.split(data); console.log(a)
