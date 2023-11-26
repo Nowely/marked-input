44f436d3f9d2c7ba314e1e354296d7552c353f34
Ruslan <ilyin-r@mail.ru>
2023-11-26 18:10:20 +0300

    Add render bench

--- tests/benchmarks/index.bench.tsx
+++ tests/benchmarks/index.bench.tsx
@@ -3,16 +3,12 @@ import {bench, describe} from 'vitest'
 describe('sort', () => {
 	bench('normal', () => {
 		const x = [1, 5, 4, 2, 3]
-		x.sort((a, b) => {
-			return a - b
-		})
+		x.sort((a, b) => a - b)
 	})
 
 	bench('reverse', () => {
 		const x = [1, 5, 4, 2, 3]
-		x.reverse().sort((a, b) => {
-			return a - b
-		})
+		x.reverse().sort((a, b) => a - b)
 
 	})
 })
