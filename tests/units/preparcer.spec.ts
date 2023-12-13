7d50b75dbb78a6cab721572db53fca300570d320
Ruslan <ilyin-r@mail.ru>
2023-12-13 08:12:09 +0300

    Update preparcer tests

--- tests/units/preparcer.spec.ts
+++ tests/units/preparcer.spec.ts
@@ -1,108 +1,82 @@
 import {expect} from 'vitest'
 
+const Hello = 'Hello'
+const Day = 'Day'
+const Great = 'Great'
+const I = 'I'
+
+const HelloDay = `${Hello}${Day}`
+const HelloGreatDay = `${Hello}${Great}${Day}`
+const HelloIDay = `${Hello}${I}${Day}`
+const HelloI = `${Hello}${I}`
+const IHello = `${I}${Hello}`
+
+//TODO process Similar start such as HEro and HEllo
 describe.only(`Utility: preparcer`, () => {
 
-	it('should work on equal', () => {
-		const gap = findGap('Hello', 'Hello')
-		expect(gap).toMatchObject({})
-	})
+	it('should work on equal', () =>
+		expect(findGap(Hello, Hello)).toMatchObject({}))
+	it('should work on insert for empty', () =>
+		expect(findGap('', Hello)).toMatchObject({left: undefined, right: undefined}))
+	it('should work on remove from empty', () =>
+		expect(findGap(Hello, '')).toMatchObject({left: 0, right: Hello.length}))
 
 
-	it('should work on insert for empty', () => {
-		const gap = findGap('', 'Hello')
-		expect(gap).toMatchObject({start: undefined, end: undefined})
-	})
+	it('should work on single start type', () =>
+		expect(findGap(Hello, IHello)).toMatchObject({left: 0, right: undefined}))
+	it('should work on multi start type', () =>
+		expect(findGap(Day, HelloDay)).toMatchObject({left: 0, right: undefined}))
 
-	it('should work on remove from empty', () => {
-		const gap = findGap('Hello', '')
-		expect(gap).toMatchObject({start: 0, end: 5})
-	})
+	it('should work on single middle type', () =>
+		expect(findGap(HelloDay, HelloIDay)).toMatchObject({left: Hello.length, right: HelloDay.length - Day.length}))
+	it('should work on multi middle type', () =>
+		expect(findGap(HelloDay, HelloGreatDay)).toMatchObject({left: Hello.length, right: HelloDay.length - Day.length}))
 
-
-	it('should work on single start type', () => {
-		const gap = findGap('Hello', '!Hello')
-		expect(gap).toMatchObject({start: 0, end: undefined})
-	})
-
-	it('should work on single middle type', () => {
-		const gap = findGap('Hello World', 'Hello! World')
-		expect(gap).toMatchObject({start: 5, end: 5})
-	})
-
-	it('should work on single end type', () => {
-		const gap = findGap('Hello World', 'Hello World!')
-		expect(gap).toMatchObject({start: undefined, end: 11})
-	})
+	it('should work on single end type', () =>
+		expect(findGap(Hello, HelloI)).toMatchObject({left: undefined, right: Hello.length}))
+	it('should work on multi end type', () =>
+		expect(findGap(Hello, HelloDay)).toMatchObject({left: undefined, right: Hello.length}))
 
 
-	it('should work on multi start type', () => {
-		const gap = findGap('World', 'Hello World')
-		expect(gap).toMatchObject({start: 0, end: undefined})
-	})
+	it('should work on single start remove', () =>
+		expect(findGap(IHello, Hello)).toMatchObject({left: 0, right: IHello.length - Hello.length}))
+	it('should work on multi start remove', () =>
+		expect(findGap(HelloDay, Day)).toMatchObject({left: 0, right: HelloDay.length - Day.length}))
 
-	it('should work on multi middle type', () => {
-		const gap = findGap('Hello World', 'Hello beautiful World')
-		expect(gap).toMatchObject({start: 6, end: 5})
-	})
+	it('should work on single middle remove', () =>
+		expect(findGap(HelloIDay, HelloDay)).toMatchObject({left: Hello.length, right: HelloI.length}))
+	it('should work on multi middle remove', () =>
+		expect(findGap(HelloGreatDay, HelloDay)).toMatchObject({
+			left: HelloGreatDay.length - HelloDay.length,
+			right: HelloGreatDay.length - Day.length
+		}))
 
-	it('should work on multi end type', () => {
-		const gap = findGap('Hello', 'Hello World')
-		expect(gap).toMatchObject({start: undefined, end: 5})
-	})
-
-
-	it('should work on single start remove', () => {
-		const gap = findGap('!Hello', 'Hello')
-		expect(gap).toMatchObject({start: 0, end: 1})
-	})
-
-	it('should work on single middle remove', () => {
-		const gap = findGap('Hello! World', 'Hello World')
-		expect(gap).toMatchObject({start: 5, end: 6})
-	})
-
-	it('should work on single end remove', () => {
-		const gap = findGap('Hello World!', 'Hello World')
-		expect(gap).toMatchObject({start: 11, end: 12})
-	})
-
-
-	it('should work on multi start remove', () => {
-		const gap = findGap('Hello World', 'World')
-		expect(gap).toMatchObject({start: 0, end: 6})
-	})
-
-	it('should work on multi middle remove', () => {
-		const gap = findGap('Hello beautiful World', 'Hello World')
-		expect(gap).toMatchObject({start: 6, end: 15})
-	})
-
-	it('should work on multi end remove', () => {
-		const gap = findGap('Hello World', 'Hello')
-		expect(gap).toMatchObject({start: 5, end: 11})
-	})
+	it('should work on single end remove', () =>
+		expect(findGap(HelloI, Hello)).toMatchObject({left: Hello.length, right: HelloI.length}))
+	it('should work on multi end remove', () =>
+		expect(findGap(HelloDay, Hello)).toMatchObject({left: Hello.length, right: HelloDay.length}))
 })
 
-export type Gap = { start?: number, end?: number }
+export type Gap = { left?: number, right?: number }
 
 function findGap(previous: string, current: string): Gap {
 	if (previous === current) return {}
 
-	let start: number | undefined
+	let left: number | undefined
 	for (let i = 0; i < previous.length; i++) {
 		if (previous[i] !== current[i]) {
-			start = i
+			left = i
 			break
 		}
 	}
 
-	let end: number | undefined
+	let right: number | undefined
 	for (let i = 1; i <= previous.length; i++) {
 		if (previous.at(-i) !== current.at(-i)) {
-			end = previous.length - i + 1
+			right = previous.length - i + 1
 			break
 		}
 	}
 
-	return {start, end}
+	return {left, right}
 }
