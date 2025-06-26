06f1c7d70620c77a0b28d238741bac1bf932e618
Nowely <ilyin-r@mail.ru>
2025-06-26 21:29:11 +0300

    Improve story readability

--- storybook/stories/Base.stories.tsx
+++ storybook/stories/Base.stories.tsx
@@ -8,14 +8,10 @@ export default {
 	title: 'MarkedInput',
 	tags: ['autodocs'],
 	component: MarkedInput,
-	args: {
-
-	}
+	args: {}
 } satisfies Meta<typeof MarkedInput>
 
-type Story = StoryObj<typeof MarkedInput<MarkStruct>>
-
-const Mark = (props: MarkStruct) => <mark onClick={_ => alert(props.value)}>{props.label}</mark>
+type Story = StoryObj<Meta<typeof MarkedInput<MarkStruct>>>
 
 export const Default: Story = {
 	args: {
@@ -41,39 +37,41 @@ const ConfiguredMarkedInput = createMarkedInput({
 	}],
 })
 
-export const Configured = () => {
-	const [value, setValue] = useState(
-		'Enter the \'@\' for calling @[primary](primary:4) suggestions and \'/\' for @[default](default:7)!\n' +
-		'Mark is can be a any component with any logic. In this example it is the @[Button](primary:54): clickable primary or secondary.\n' +
-		'For found mark used @[annotations](default:123).'
-	)
+export const Configured: Story = {
+	render: () => {
+		const [value, setValue] = useState(
+			'Enter the \'@\' for calling @[primary](primary:4) suggestions and \'/\' for @[default](default:7)!\n' +
+			'Mark is can be a any component with any logic. In this example it is the @[Button](primary:54): clickable primary or secondary.\n' +
+			'For found mark used @[annotations](default:123).'
+		)
 
-	const displayText = denote(value, mark => mark.label, PrimaryMarkup, DefaultMarkup)
+		const displayText = denote(value, mark => mark.label, PrimaryMarkup, DefaultMarkup)
 
-	return (
-		<>
-			<ConfiguredMarkedInput value={value} onChange={setValue}>
-				<div
-					onClick={_ => console.log('onCLick')}
-					onInput={_ => console.log('onInput')}
-					onBlur={_ => console.log('onBlur')}
-					onFocus={_ => console.log('onFocus')}
-					onKeyDown={_ => console.log('onKeyDown')}
-				/>
-			</ConfiguredMarkedInput>
+		return (
+			<>
+				<ConfiguredMarkedInput value={value} onChange={setValue}>
+					<div
+						onClick={_ => console.log('onCLick')}
+						onInput={_ => console.log('onInput')}
+						onBlur={_ => console.log('onBlur')}
+						onFocus={_ => console.log('onFocus')}
+						onKeyDown={_ => console.log('onKeyDown')}
+					/>
+				</ConfiguredMarkedInput>
 
-			<Text label="Plaint text:" value={value}/>
-			<Text label="Display text (denoted):" value={displayText}/>
-		</>
-	)
+				<Text label="Plaint text:" value={value}/>
+				<Text label="Display text (denoted):" value={displayText}/>
+			</>
+		)
+	}
 }
 
-export const Autocomplete = () => {
-	const [value, setValue] = useState('Hello, clickable marked @world!')
-	return (
-		<MarkedInput value={value} onChange={setValue} options={[{
+export const Autocomplete: Story = {
+	args: {
+		defaultValue: 'Hello, clickable marked @world!',
+		options: [{
 			markup: '@__label__',
 			data: ['one', 'two', 'three', 'four']
-		}]}/>
-	)
+		}]
+	}
 }
