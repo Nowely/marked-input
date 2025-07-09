91f2b78f7dd708bbb5b49c54740874f3a933ae44
Nowely <ilyin-r@mail.ru>
2025-07-09 22:36:35 +0300

    Clear

--- lib/features/default/constants.ts
+++ lib/features/default/constants.ts
@@ -4,7 +4,6 @@ export const DEFAULT_TRIGGER = '@'
 
 export const DEFAULT_MARKUP = '@[__label__](__value__)'
 
-//TODO collaps to parsed function
 export const DEFAULT_OPTIONS: InnerOption[] = [{
 	trigger: DEFAULT_TRIGGER,
 	markup: DEFAULT_MARKUP,
