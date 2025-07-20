3a130547fc3a0eb7e2c705db4a73dcc2f60a8f15
Nowely <ilyin-r@mail.ru>
2025-07-20 23:45:15 +0300

    Migrate to pnpm. Step 2

--- tests/integrations/MarkedInputHandler.spec.tsx
+++ tests/integrations/MarkedInputHandler.spec.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import {render} from '@testing-library/react'
 import {MarkedInputHandler} from 'rc-marked-input'
-import {Story} from '../_utils/stories'
+import {Story} from '../../storybook/stories'
 
 const {Default} = Story.Base
 
--- tests/integrations/stories.spec.tsx
+++ tests/integrations/stories.spec.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import {render} from '@testing-library/react'
 import {describe, expect} from 'vitest'
-import {Story} from '../_utils/stories'
+import {Story} from '../../storybook/stories'
 
 //TODO correct type
 const getTests = ([name, Story]: [string, any]) =>
