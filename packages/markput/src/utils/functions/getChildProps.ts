f8fe8dec8b3af1a535c5fcbd27b7215b6b1256d1
Nowely <ilyin-r@mail.ru>
2025-08-14 00:04:09 +0300

    Clear

--- packages/markput/src/utils/functions/getChildProps.ts
+++ packages/markput/src/utils/functions/getChildProps.ts
@@ -1,5 +1,5 @@
 import {Children} from 'react'
-import type {Store} from '@markput/core/src/utils/classes/Store'
+import type {Store} from '@markput/core'
 
 export const getChildProps = (type: string) => (state: Store) =>
-	Children.map(state.props.children, child => child)?.find(child => child.type === type)?.props
+	Children.map(state.props.children, child => child)?.find((child: {type: string}) => child.type === type)?.props
