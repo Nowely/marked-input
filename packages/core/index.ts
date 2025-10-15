c85b5ef5a7072183abbc129bf96d8ce3bc2165cc
Nowely <ilyin-r@mail.ru>
2025-10-15 19:00:27 +0300

    Refactor exports in index.ts for improved clarity and organization
    - Removed unnecessary exports of label, value, and MarkMatch types, consolidating them under Parser/types.
    - Updated the export structure for Parser and ParserV2 to enhance readability and maintainability.

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,9 +2,6 @@
 export {assertAnnotated, assertNonNullable, isAnnotated} from './src/shared/checkers'
 export {KEYBOARD} from './src/shared/constants'
 export type {
-	label,
-	value,
-	MarkMatch,
 	MarkStruct,
 	OverlayMatch,
 	EventKey,
@@ -15,8 +12,9 @@ export type {
 // Feature exports
 export type {InnerOption, InnerMarkedInputProps} from './src/features/default'
 export {DEFAULT_CLASS_NAME, DEFAULT_MARKUP, DEFAULT_OPTIONS, DEFAULT_TRIGGER} from './src/features/default'
-export {Parser, ParserV2, annotate, escape, denote, getTokensByValue, getTokensByUI} from './src/features/parsing'
-export type {Markup as ParserMarkup} from './src/features/parsing/Parser'
+export {Parser, annotate, escape, denote, getTokensByValue, getTokensByUI} from './src/features/parsing'
+export {ParserV2} from './src/features/parsing/ParserV2'
+export type {Markup as ParserMarkup, label, value, MarkMatch} from './src/features/parsing/Parser/types'
 export type {Markup as ParserV2Markup} from './src/features/parsing/ParserV2'
 export {findGap, getClosestIndexes} from './src/features/preparsing'
 export {toString, shallow, createNewSpan, deleteMark} from './src/features/text-manipulation'
