400daf881354f159281819cf6456e2f42ab3496f
Nowely <ilyin-r@mail.ru>
2025-11-29 22:33:24 +0300

    Refactor Storybook Vite configuration and remove Vitest config file
    - Updated Vite configuration to include testing setup with Vitest, specifying globals, environment, and coverage options.
    - Removed the deprecated Vitest configuration file to streamline the project structure.

--- packages/storybook/vite.config.ts
+++ packages/storybook/vite.config.ts
@@ -1,6 +1,15 @@
 import react from '@vitejs/plugin-react-swc'
-import {defineConfig} from 'vite'
+import {defineConfig} from 'vitest/config'
 
 export default defineConfig({
 	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		include: ['src/pages/**/*.spec.ts', 'src/pages/**/*.spec.tsx'],
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'json', 'html'],
+		},
+	},
 })
