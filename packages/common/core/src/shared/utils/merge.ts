02738dda191c7e50f35e02793d57c403b9312d7b
Nowely <ilyin-r@mail.ru>
2026-02-22 13:13:15 +0300

    refactor(core): simplify merge using spread syntax

--- packages/common/core/src/shared/utils/merge.ts
+++ packages/common/core/src/shared/utils/merge.ts
@@ -1,10 +1,4 @@
 export function merge<T extends object>(...objects: (T | undefined | null | false)[]): T | undefined {
-	const result = {} as T
-	for (const obj of objects) {
-		if (obj) {
-			Object.assign(result, obj)
-		}
-	}
-
+	const result = Object.assign({}, ...objects.filter(Boolean))
 	return Object.keys(result).length > 0 ? result : undefined
 }
