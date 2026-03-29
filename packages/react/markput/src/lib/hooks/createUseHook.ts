615126c6a96e2491b84da8126f3d0aad69065385
Nowely <ilyin-r@mail.ru>
2026-03-30 00:27:15 +0300

    fix(hooks): update createUseHook to include getSnapshot in useSyncExternalStore
    - Modified the createUseHook function to pass getSnapshot as a third argument to useSyncExternalStore, ensuring consistent state retrieval and improving performance.

--- packages/react/markput/src/lib/hooks/createUseHook.ts
+++ packages/react/markput/src/lib/hooks/createUseHook.ts
@@ -4,5 +4,5 @@ import {useSyncExternalStore} from 'react'
 export const createUseHook = <T>(signal: Signal<T>) => {
 	const subscribe = (callback: () => void) => signal.on(callback)
 	const getSnapshot = () => signal.get()
-	return () => useSyncExternalStore(subscribe, getSnapshot)
+	return () => useSyncExternalStore(subscribe, getSnapshot, getSnapshot)
 }
