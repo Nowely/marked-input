16fa3afc1825dede1f0c79b5a942e934135b9010
Nowely <ilyin-r@mail.ru>
2026-04-04 23:50:13 +0300

    fix(storybook): remove eslint-disable, add oxlint-disable for unsafe-call in vitest.workspace.ts

--- packages/storybook/vitest.workspace.ts
+++ packages/storybook/vitest.workspace.ts
@@ -3,21 +3,19 @@ import vue from '@vitejs/plugin-vue'
 import {playwright} from '@vitest/browser-playwright'
 import {defineWorkspace} from 'vitest/config'
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const browser = {
 	enabled: true,
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+	// oxlint-disable-next-line typescript-eslint/no-unsafe-call
 	provider: playwright(),
 	instances: [{browser: 'chromium'}],
 	viewport: {width: 1280, height: 720},
 	headless: true,
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+// oxlint-disable-next-line typescript-eslint/no-unsafe-call
 export default defineWorkspace([
 	{
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+		// oxlint-disable-next-line typescript-eslint/no-unsafe-call
 		plugins: [react()],
 		resolve: {dedupe: ['react', 'react-dom']},
 		test: {
@@ -30,7 +28,7 @@ export default defineWorkspace([
 		},
 	},
 	{
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+		// oxlint-disable-next-line typescript-eslint/no-unsafe-call
 		plugins: [vue()],
 		resolve: {dedupe: ['vue']},
 		test: {
